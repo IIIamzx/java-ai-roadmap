904dc1d47508b23a96b03353f5141a0779dd1a46
zhangxin <1735911125@qq.com>
2025-03-01 23:24:20 +0800

    依赖构建

---
+++ spring-ai/src/main/java/com/java/ai/SpringAiApplication.java
@@ -0,0 +1,12 @@
+package com.java.ai;
+
+/**
+ * @author: zhangxin
+ * @date: 2025/3/1
+ * @description:
+ */
+public class SpringAiApplication {
+    public static void main(String[] args) {
+
+    }
+}
