3e0771642d55485f74a558262d8e715374192dc5
zhangxin <1735911125@qq.com>
2025-03-13 20:34:58 +0800

    langchain4j: 初始化配置-调用ali百炼平台

---
+++ langchain4j-ai/src/main/java/com/java/langchain4j/Langchain4jApplication.java
@@ -0,0 +1,16 @@
+package com.java.langchain4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: zhangxin
+ * @date: 2025/3/1
+ * @description:
+ */
+@SpringBootApplication
+public class Langchain4jApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Langchain4jApplication.class,args);
+    }
+}
---
+++ langchain4j-ai/src/main/java/com/java/langchain4j/controller/ChatController.java
@@ -0,0 +1,36 @@
+package com.java.langchain4j.controller;
+
+import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.model.chat.ChatLanguageModel;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: zhangxin
+ * @date: 2025/3/13
+ * @description:
+ */
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/langchain4j")
+public class ChatController {
+
+    public static final Logger logger = LoggerFactory.getLogger(ChatController.class);
+
+    final ChatLanguageModel chatLanguageModel;
+
+    @GetMapping("/chat")
+    public String chat(@RequestParam("message") String msg){
+        logger.info("【用户输入】：{}",msg);
+        String ans = chatLanguageModel.chat(UserMessage.from(msg)).aiMessage().text();
+        logger.info("【模型回答】：{}",ans);
+        return ans;
+    }
+}
